8364b01e9dae5af2ae8b637377a7cc18d38afe9a
Chandler Dance <dan20005@byui.edu>
2024-04-27 21:47:42 -0600

    Core Requirement 2 for Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -28,6 +28,14 @@ class Program
         {
             Console.WriteLine("Your Got an F.");
         }
+        if (gradeCPD >= 70)
+        {
+            Console.WriteLine("Congradulations! You Passed!!!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, You Failed.  Better Luck Next Time!");
+        }
     }
 }
 
@@ -46,8 +54,6 @@ Start by completing the core requirements, then when that part is complete, if y
 Please work through the requirements in order rather than jumping ahead to more complicated steps, to ensure that everyone is following the concepts.
 
 Core Requirements
-Ask the user for their grade percentage, then write a series of if-elif-else statements to print out the appropriate letter grade. (At this point, you'll have a separate print statement for each grade letter in the appropriate block.)
-
 Assume that you must have at least a 70 to pass the class. After determining the letter grade and printing it out. Add a separate if statement to determine if the user passed the course, and if so display a message to congratulate them. If not, display a different message to encourage them for next time.
 
 Change your code from the first part, so that instead of printing the letter grade in the body of each if, elif, or else block, instead create a new variable called letter and then in each block, set this variable to the appropriate value. Finally, after the whole series of if-elif-else statements, have a single print statement that prints the letter grade once.
