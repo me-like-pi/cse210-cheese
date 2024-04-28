4050f102197c5549b48b000660db9b6617dc641d
Chandler Dance <dan20005@byui.edu>
2024-04-27 19:57:20 -0600

    Notes for In Class

---
+++ sandbox/Sandbox/Notes.cs
@@ -0,0 +1,11 @@
+//Python            C#
+
+//#comment          //comment
+//Darren = 5,string,bool   bool me = bool, int 5 = 5, float(double) 2.5 = 2.5
+//input(prompt)  
+
+
+
+// if x>0:        if (x>0) {}
+// elif x<0:      else if () {}
+// else:          else {}
