182681b26c10aa6c880fa45fdeef34be0a313aab
Chandler Dance <dan20005@byui.edu>
2024-04-27 21:15:03 -0600

    Prep 1 Assignment Completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine();
+        Console.WriteLine("What is your first name?");
+        string FirstNameCPD = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string LastNameCPD = Console.ReadLine();
+        Console.WriteLine($"Your name is {LastNameCPD}, {FirstNameCPD} {LastNameCPD}.");
     }
 }
