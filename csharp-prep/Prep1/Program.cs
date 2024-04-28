b605dc44a533c3419cdc28903a23b8e7e438c734
Chandler Dance <dan20005@byui.edu>
2024-04-27 21:19:13 -0600

    Changed var names to comply with style code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-        string FirstNameCPD = Console.ReadLine();
+        string firstNameCPD = Console.ReadLine();
         Console.WriteLine("What is your last name?");
-        string LastNameCPD = Console.ReadLine();
+        string lastNameCPD = Console.ReadLine();
         Console.WriteLine($"Your name is {LastNameCPD}, {FirstNameCPD} {LastNameCPD}.");
     }
 }
