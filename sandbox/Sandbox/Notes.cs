ddcdbcb39bc09695c9ab847303e8709212fbc263
Chandler Dance <dan20005@byui.edu>
2024-04-27 21:16:14 -0600

    more Notes

--- sandbox/Sandbox/Notes.cs
+++ sandbox/Sandbox/Notes.cs
@@ -1,12 +1,12 @@
-//Python                    C#
+//Python                                        C#
 
-//#comment                  //comment
-//Darren = 5,string,bool    bool me = bool, int 5 = 5, float(double) 2.5 = 2.5
-//input(prompt)             Console.Write("What is your favorite color? ");
-//                          string color = Console.ReadLine();
+//#comment                                      //comment
+//Darren = 5,string,bool                        bool me = bool, int 5 = 5, float(double) 2.5 = 2.5
+//input(prompt)                                 Console.Write("What is your favorite color? ");
+//                                              string color = Console.ReadLine();
+//print(f"I am studying at {school}.")          Console.WriteLine($"I am studying at {school}.");               
 
 
-
-// if x>0:                  if (x>0) {}
-// elif x<0:                else if () {}
-// else:                    else {}
+// if x>0:                                      if (x>0) {}
+// elif x<0:                                    else if () {}
+// else:                                        else {}
