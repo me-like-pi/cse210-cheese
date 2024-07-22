2a2b3f6fd0867a661f9039ae99acd46c199ba813
Chandler Dance <dan20005@byui.edu>
2024-07-22 08:33:56 -0600

    Foundation1 Finished

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,19 +1,69 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+public class Program
 {
     public static void Main()
     {
         Console.WriteLine("Hello Foundation1 World!");
 
-        Video video = new Video
+        List<Video> videos = new List<Video>
         {
-            Title = "I ate 100000 chicken nuggets",
-            Author = "xXgamermanXx",
-            Length = 120,
+            new Video
+            {
+                Title = "I ate 100000 chicken nuggets",
+                Author = "xXgamermanXx",
+                Length = 120
+            },
+            new Video
+            {
+                Title = "Speedrunning Minecraft in 10 minutes",
+                Author = "speedrunMaster",
+                Length = 15
+            },
+            new Video
+            {
+                Title = "How to cook the perfect steak",
+                Author = "Master Chef",
+                Length = 45
+            },
+            new Video
+            {
+                Title = "Top 10 travel destinations for 2024",
+                Author = "TravelDan",
+                Length = 180
+            }
         };
-        video.AddComment("man that was probs too many ngl!");
-        Console.WriteLine($"Video: {video.GetTitle()}, Author: {video.GetAuthor()}, Comments: {video.GetNumberOfComments()}");
+
+        videos[0].AddComment("John Halo", "man that was probs too many ngl!");
+        videos[0].AddComment("Melikpi", "Epic video!");
+        videos[0].AddComment("taulover", "I'm hungry now.");
+
+        videos[1].AddComment("Dream", "Amazing run!");
+        videos[1].AddComment("Pewdiepie", "How did you do that?");
+        videos[1].AddComment("urmum6969696", "Incredible!");
+
+        videos[2].AddComment("lookbehindyou", "That looks delicious!");
+        videos[2].AddComment("master cheeks", "Great tips.");
+        videos[2].AddComment("burgrlovr", "Can't wait to try this!");
+
+        videos[3].AddComment("ihvaqustn", "I want to go to all these places!");
+        videos[3].AddComment("tom", "Great suggestions.");
+        videos[3].AddComment("jerry", "Thanks for the info!");
+
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Video: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAuthor()}");
+            Console.WriteLine($"Length: {video.GetLength()} minutes");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            foreach (var comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetCommentText()}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,12 +3,15 @@ public class Video
     public string Title { get; set; }
     public string Author { get; set; }
     public int Length { get; set; }
-    public List<string> Comments { get; set; } = new List<string>();
+    public List<Comment> Comments { get; set; } = new List<Comment>();
 
     public string GetTitle() => Title;
     public string GetAuthor() => Author;
     public int GetLength() => Length;
     public int GetNumberOfComments() => Comments.Count;
-    public void AddComment(string comment) => Comments.Add(comment);
-    public List<string> GetComments() => Comments;
+    public void AddComment(string commenterName, string commenterText) 
+    {
+        Comments.Add(new Comment { CommenterName = commenterName, CommenterText = commenterText });
+    }
+    public List<Comment> GetComments() => Comments;
 }
